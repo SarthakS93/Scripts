60e57c0f16c8e2bee38b344eaf6a637993bac89f
sarthak <sarthakS93@gmail.com>
2016-02-25 02:04:07 +0530

    changed Search.py to work with python3 and included use of hassle free requests package, also changed the output to be more readable

--- Search.py
+++ Search.py
@@ -1,12 +1,10 @@
-import urllib
-import json as m_json
-Query = raw_input('Query: ')
-Query = urllib.urlencode({'q': Query})
-response = urllib.urlopen(
-    'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + Query).read()
-json = m_json.loads(response)
-results = json['responseData']['results']
-for result in results:
-    title = result['title']
-    url = result['url']
-    print (title + '; ' + url)
+import requests, json
+Query = input('Query: ')
+p = {'q' : Query}
+response = requests.get('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&', params = p)
+j = response.json()
+results = j['responseData']['results']
+for r in results:
+	title = r['titleNoFormatting']
+	url = r['url']
+	print(title + ' ; ' + url)
